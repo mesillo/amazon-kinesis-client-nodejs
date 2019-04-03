78ae9d7f02431aacd0bf43679b88f06d5d1b71ec
MesillO <mesin.alberto@gmail.com>
2019-04-03 11:38:30 +0200

    tabs and ident.

--- samples/basic_sample/consumer/consumer.js
+++ samples/basic_sample/consumer/consumer.js
@@ -3,17 +3,17 @@
 var AWS = require('aws-sdk');
 
 var config = {
-    kinesis : {
-      region : 'eu-west-1',
-      endpoint: "http://localhost:4568",
-      accessKeyId:  "fakeAccessKeyId",
-      secretAccessKey: "fakeSecretAccessKey",
-      sessionToken: "fakeSessionToken"
-    },
+	kinesis : {
+	  region : 'eu-west-1',
+	  endpoint: "http://localhost:4568",
+	  accessKeyId:  "fakeAccessKeyId",
+	  secretAccessKey: "fakeSecretAccessKey",
+	  sessionToken: "fakeSessionToken"
+	},
 
-    sampleConsume : {
-        stream : 'iot-raw-messages'
-    }
+	sampleConsume : {
+		stream : 'iot-raw-messages'
+	}
 };
 
 var kinesis = new AWS.Kinesis( config.kinesis );
@@ -23,31 +23,31 @@ kinesis.describeStream({
   StreamName: config.sampleConsume.stream
 }, function(err, streamData) {
   if (err) {
-    console.log(err, err.stack); // an error occurred
+	console.log(err, err.stack); // an error occurred
   } else {
-    console.log(streamData); // successful response
-    streamData.StreamDescription.Shards.forEach(shard => {
-      kinesis.getShardIterator({
-        ShardId: shard.ShardId,
-        ShardIteratorType: 'TRIM_HORIZON',
-        //StreamName: 'cloudonaut-stream'
-        StreamName: config.sampleConsume.stream
-      }, function(err, shardIteratordata) {
-        if (err) {
-          console.log(err, err.stack); // an error occurred
-        } else {
-          console.log(shardIteratordata); // successful response
-          kinesis.getRecords({
-            ShardIterator: shardIteratordata.ShardIterator
-          }, function(err, recordsData) {
-            if (err) {
-              console.log(err, err.stack); // an error occurred
-            } else {
-              console.log(recordsData); // successful response
-            }
-          });
-        }
-      });
-    });
+	console.log(streamData); // successful response
+	streamData.StreamDescription.Shards.forEach(shard => {
+	  kinesis.getShardIterator({
+		ShardId: shard.ShardId,
+		ShardIteratorType: 'TRIM_HORIZON',
+		//StreamName: 'cloudonaut-stream'
+		StreamName: config.sampleConsume.stream
+	  }, function(err, shardIteratordata) {
+		if (err) {
+		  console.log(err, err.stack); // an error occurred
+		} else {
+		  console.log(shardIteratordata); // successful response
+		  kinesis.getRecords({
+			ShardIterator: shardIteratordata.ShardIterator
+		  }, function(err, recordsData) {
+			if (err) {
+			  console.log(err, err.stack); // an error occurred
+			} else {
+			  console.log(recordsData); // successful response
+			}
+		  });
+		}
+	  });
+	});
   }
 });
